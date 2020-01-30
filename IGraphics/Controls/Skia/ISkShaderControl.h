8650376b12dc0d664dbc01f40c8f6a8a6ac1c885
oli <olilarkin@googlemail.com>
2020-01-30 23:28:57 +0000

    improve shader control

--- IGraphics/Controls/Skia/ISkShaderControl.h
+++ IGraphics/Controls/Skia/ISkShaderControl.h
@@ -16,8 +16,9 @@
  */
 
 #include "IControl.h"
-#include "src/core/SkRuntimeEffect.h"
+#include "SkRuntimeEffect.h"
 #include "SkGradientShader.h"
+#include "IGraphicsSkia.h"
 
 BEGIN_IPLUG_NAMESPACE
 BEGIN_IGRAPHICS_NAMESPACE
@@ -31,25 +32,48 @@ public:
   ISkShaderControl(const IRECT& bounds)
   : IControl(bounds)
   {
-    fSkSL =
-        "uniform float time;\n"
-        "void main(float x, float y, inout half4 color) {\n"
-          "half timeh = half(time);\n"
-          "color = half4(timeh, half(y)/300, timeh, half(mod(y,10)));"
-        "}\n";
+    auto data = SkData::MakeFromFileName("C:\\Users\\oli\\Desktop\\SkShader.frag");
+    fSkSL = SkString((const char*)data->bytes(), data->size());
+
+    DBGMSG(fSkSL.c_str());
+
+    //data->
+//    fSkSL =
+//"uniform float rad_scale;\n"
+//"uniform float2 in_center;\n"
+//"\n"
+//"void main(float x, float y, inout half4 color) {\n"
+//"float xx = x - in_center.x;\n"
+//"float yy = y - in_center.y;\n"
+//"float radius = sqrt(xx * xx + yy * yy);\n"
+//"radius = sqrt(radius);\n"
+//"float angle = atan(yy / xx);\n"
+//"float t = (angle + 3.1415926 / 2) / (3.1415926);\n"
+//"t += radius * rad_scale;\n"
+//"t = fract(t);\n"
+//"color = half4(mix(half4(1,0,0,1), half4(0,1,0,1), t));\n"
+//"}\n";
+
+        //"uniform float time;\n"
+        //"void main(float x, float y, inout half4 color) {\n"
+        //  "half timeh = half(time);\n"
+        //  "color = half4(timeh, half(y)/300, timeh, half(mod(y,10)));"
+        //"}\n";
 
     SetActionFunction([&](IControl* pCaller) {
 
       SetAnimation([&](IControl* pCaller) {
 
-        float* pInputLoc = (float*)fInputs.get();
-        *pInputLoc = (float)pCaller->GetAnimationProgress();
+        float* pInputLoc = (float*) fInputs.get();
+        pInputLoc[0] = std::sin((float) pCaller->GetAnimationProgress() * 10.f) / 5;
+        pInputLoc[1] = pCaller->GetRECT().MW();
+        pInputLoc[2] = pCaller->GetRECT().MH();
 
         if (pCaller->GetAnimationProgress() > 1.) {
           pCaller->OnEndAnimation();
         }
 
-      }, 1000);
+      }, 10000);
 
     });
 
@@ -67,13 +91,7 @@ public:
   void DoDraw(SkCanvas* canvas)
   {
     canvas->clear(SK_ColorWHITE);
-
-    auto inputs = SkData::MakeWithoutCopy(fInputs.get(), fEffect->inputSize());
-    auto shader = fEffect->makeShader(std::move(inputs), fChildren.data(), fChildren.count(), nullptr, false);
-
-    SkPaint p;
-    p.setShader(std::move(shader));
-    canvas->drawRect({ mRECT.L, mRECT.T, mRECT.W(), mRECT.H() }, p);
+    canvas->drawRect(SkiaRect(mRECT.GetPadded(1)), fPaint);
   }
 
   void OnMouseDown(float x, float y, const IMouseMod& mod) override
@@ -83,10 +101,10 @@ public:
   
   void Load(SkScalar winWidth, SkScalar winHeight)
   {
-    SkPoint points[] = { { 0, 0 }, { winWidth, 0 } };
-    SkColor colors[] = { SK_ColorRED, SK_ColorGREEN };
+    //SkPoint points[] = { { 0, 0 }, { winWidth, 0 } };
+    //SkColor colors[] = { SK_ColorRED, SK_ColorGREEN };
 
-    sk_sp<SkShader> shader;
+    //sk_sp<SkShader> shader;
 
 //    shader = SkGradientShader::MakeLinear(points, colors, nullptr, 2, SkTileMode::kClamp);
 //    fShaders.push_back(std::make_pair("Linear Gradient", shader));
@@ -105,7 +123,7 @@ public:
   {
     auto [effect, errorText] = SkRuntimeEffect::Make(fSkSL);
     if (!effect) {
-      printf("%s\n", errorText.c_str());
+      DBGMSG("%s\n", errorText.c_str());
       return false;
     }
 
@@ -114,7 +132,7 @@ public:
     if (effect->inputSize() > oldSize) {
         memset(fInputs.get() + oldSize, 0, effect->inputSize() - oldSize);
     }
-    fChildren.resize_back(effect->childCount());
+    fChildren.resize_back(effect->children().count());
     for (auto& c : fChildren) {
         if (!c) {
             c = fShaders[0].second;
@@ -122,6 +140,11 @@ public:
     }
 
     fEffect = effect;
+
+    auto inputs = SkData::MakeWithoutCopy(fInputs.get(), fEffect->inputSize());
+    auto shader = fEffect->makeShader(std::move(inputs), fChildren.data(), fChildren.count(), nullptr, false);
+    fPaint.setShader(std::move(shader));
+
     return true;
   }
   
@@ -130,7 +153,7 @@ private:
   SkAutoTMalloc<char> fInputs;
   SkTArray<sk_sp<SkShader>> fChildren;
   SkString fSkSL;
-  
+  SkPaint fPaint;
   // Named shaders that can be selected as inputs
   SkTArray<std::pair<const char*, sk_sp<SkShader>>> fShaders;
 };
