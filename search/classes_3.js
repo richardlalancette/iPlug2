f1be4b15d500468f464c97b6be4830b1e1924516
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-17 23:34:44 +0000

    Deploy iPlug2/iPlug2 to github.com/iPlug2/iPlug2.git:gh-pages

---
+++ search/classes_3.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['data',['Data',['../struct_i_l_e_d_control_1_1_data.html',1,'ILEDControl']]],
+  ['data',['Data',['../struct_i_v_scope_control_1_1_data.html',1,'IVScopeControl']]],
+  ['data',['Data',['../struct_i_v_meter_control_1_1_data.html',1,'IVMeterControl']]]
+];
